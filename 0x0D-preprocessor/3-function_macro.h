148396a03a1ded74c275b433ab22c6be7c778837
Jackson910 <jacksonmwamba681@gmail.com>
2023-07-17 06:25:01 -0700

    C preprocesser

---
+++ 0x0D-preprocessor/3-function_macro.h
@@ -0,0 +1,9 @@
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
+
+#ifndef ABS
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif /* ABS */
+
+#endif /* 3_FUNCTION_LIKE_MACRO_H */
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef SUM_H
+#define SUM_H
+
+#ifndef SUM
+#define SUM(x, y) ((x) + (y))
+#endif /* SUM */
+
+#endif /* SUM_H */
